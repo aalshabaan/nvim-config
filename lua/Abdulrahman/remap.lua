623c2368b4aa44aa4b6ec069dad59a62fa9781a7
Abed Alrahman Shabaan, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2023-12-18 07:10:44 +0100

    Change working dir to current dir

--- lua/Abdulrahman/remap.lua
+++ lua/Abdulrahman/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>v", vim.cmd.Vex)
+vim.keymap.set("n", "<leader>cd", vim.cmd('cd %:h'))
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<leader>y", "\"+y")
