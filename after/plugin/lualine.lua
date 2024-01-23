c5a28e11cc7bcdd22a11148f396ab252c513406e
Abed Alrahman Shabaan, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-01-23 17:31:38 +0100

    Added reminder of death and the afterlife

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,8 @@
 require('lualine').setup({
 	sections = {
-		lualine_a = {'buffers'}
+		lualine_a = {'buffers'},
+		lualine_c = {function ()
+			return 'واتقوا يوماً ترجعون فيه إلى الله ثم توفى كل نفس ما كسبت وهم لا يظلمون'
+		end}
 	}
 })
