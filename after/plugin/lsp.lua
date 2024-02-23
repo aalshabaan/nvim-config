2d829b1848b5ce78198038c0515d291429983676
Abed Alrahman Shaban, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-02-23 08:23:03 +0000

    Added inc_rename

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,12 +3,6 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 lsp.setup()
 
--- lsp.on_attach(function(_, bufnr)
---   -- see :help lsp-zero-keybindings
---   -- to learn the available actions
---   lsp.default_keymaps({buffer = bufnr})
--- end)
-
 lsp.on_attach(function(_, bufnr)
   local opts = {buffer = bufnr, remap = false}
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
@@ -19,7 +13,7 @@ lsp.on_attach(function(_, bufnr)
   vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
   vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
   vim.keymap.set("n", "<leader>vr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>rn", ":IncRename ", opts)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
 
