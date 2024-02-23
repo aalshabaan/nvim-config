6cbea9cf3dd64b205cb2aaece010bd129c500cbd
Abed Alrahman Shaban, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-02-23 08:23:20 +0000

    Live grep instead of boring grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,13 +3,7 @@ local telescope = require('telescope')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fs', function ()
-    local input = vim.fn.input "Grep > "
-    if string.len(input) == 0 then
-        return
-    end
-	builtin.grep_string({ search = input })
-end)
+vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
 
 telescope.setup({
 	file_ignore_patterns = {
