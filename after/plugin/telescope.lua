d62a14ed02cf59c09549c456489d188959477f40
Abed Alrahman Shabaan, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-02-26 14:32:58 +0100

    Ignore some stuff in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,4 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
@@ -5,3 +6,12 @@ vim.keymap.set('n', '<leader>fs', function ()
 	builtin.grep_string({ search = vim.fn.input "Grep > "})
 end)
 vim.keymap.set('n', '<leader>fb', builtin.buffers)
+
+
+telescope.setup({
+	file_ignore_patterns = {
+		'node%_modules',
+		'target',
+		'site-packages'
+	}
+})
