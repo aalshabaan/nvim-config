4f4f86bcd992bd386d40ce8a595a113496525807
Abed Alrahman Shaban, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-04-30 09:44:41 +0000

    Just split instead of opening netrw

--- lua/Abdulrahman/remap.lua
+++ lua/Abdulrahman/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>v", vim.cmd.Vex)
+vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>p", "\"+p")
 vim.keymap.set("n", "<leader>P", "\"+P")
