17da39d53c1832cae81280446662e51d654f5e7a
Abed Alrahman Shaban, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-05-06 10:00:11 +0000

    Add diagnostics back

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,6 +1,6 @@
 require('lualine').setup({
 	sections = {
-        lualine_b = {'branch', 'filename', 'diff'},
+        lualine_b = {'branch', 'filename', 'diff', 'diagnostics'},
 		lualine_c = {function ()
 			return 'And fear a day in which you shall return to God, then every soul will be given the full due of what it has done, and no injustice will be done unto them'
         end}
