7d947e1763046ab0703ed6b175dcba84602e7263
Abed Alrahman Shabaan, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-06-13 10:06:18 +0200

    Changed Git branch view from Fugitive to Telescope (I love Telescope at this point)

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,8 +2,12 @@ local builtin = require('telescope.builtin')
 local telescope = require('telescope')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers)
+vim.keymap.set('n', '<leader>gb', builtin.git_branches)
+vim.keymap.set('n', '<leader>gc', builtin.git_commits)
+vim.keymap.set('n', '<leader>bc', builtin.git_bcommits)
+
 
 telescope.setup({
 	file_ignore_patterns = {
