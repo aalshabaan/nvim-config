f74fa6f75d18ae252d352cb2c6e6c2e9ff7d847d
Abed Alrahman Shabaan, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-08-06 10:16:54 +0200

    More Altravis file extensions

--- lua/Abdulrahman/altravis.lua
+++ lua/Abdulrahman/altravis.lua
@@ -14,6 +14,7 @@ vim.filetype.add({
         reports = 'json',
         rules = 'lua',
         template = 'json',
+        maps = 'json',
         ["network-monitoring"] = 'json',
         lookup = 'csv'
     },
