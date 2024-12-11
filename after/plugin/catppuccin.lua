745911fc2fd4d026031ec363dd2ec927000d8ff9
Abed Alrahman Shabaan, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-12-11 16:14:34 +0100

    Remove unused value from Catppuccin value (because of lualine)

--- after/plugin/catppuccin.lua
+++ after/plugin/catppuccin.lua
@@ -1,13 +1,13 @@
 require("catppuccin").setup {
-	highlight_overrides = {
-		all = function (colors)
-			return {
-				LineNr = {fg = "#e0351f"},
-				CmpBorder = {fg = colors.grey},
-			}
-		end
-	},
-	transparent_background = true,
-	show_end_of_buffers = false
+    highlight_overrides = {
+        all = function(colors)
+            return {
+                LineNr = { fg = "#e0351f" },
+                CmpBorder = { fg = colors.grey },
+            }
+        end
+    },
+    transparent_background = true,
+    show_end_of_buffers = false
 }
 
