c56eadbb64b358a3c38b5beaad762ad4fb62fe5e
Abed Alrahman Shabaan, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-12-17 12:00:43 +0100

    Show diagnostics in qf list

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,17 +10,20 @@ lspzero.setup()
 -- end)
 
 lspzero.on_attach(function(_, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>vr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>rn", ":IncRename ", opts)
-  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    local opts = {buffer = bufnr, remap = false}
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>vr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>rn", ":IncRename ", opts)
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<leader>dd", function ()
+        vim.diagnostic.setqflist(vim.diagnostic.toqflist(vim.diagnostic.get()))
+    end, opts)
 end)
 
 require('mason').setup()
@@ -40,4 +43,3 @@ lspzero.new_client({
 	  return vim.fn.getcwd()
   end,
 })
-
