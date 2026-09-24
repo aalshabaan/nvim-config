a39ca7d03074b94bee282d9d11ee5c67236b6f83
Abed Alrahman Shabaan <abed.shabaan@salt.ch>
2026-09-24 16:26:55 +0200

    Display jsonpath in lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,9 +1,16 @@
+local json = require('jsonpath')
+
 require('lualine').setup({
-	sections = {
-        lualine_a = {'mode',},
-        lualine_b = {'branch', 'filename', 'diff'},
-		lualine_c = {function ()
-			return 'And fear a day in which you shall return to God, then every soul will be given the full due of what it has done, and no injustice will be done unto them'
-		end}
-	}
+    sections = {
+        lualine_a = { 'mode', },
+        lualine_b = { 'branch', 'filename', 'diff' },
+        lualine_c = { function()
+            if json and vim.bo.filetype == 'json' then
+                return json.get()
+            else
+                return
+                'And fear a day in which you shall return to God, then every soul will be given the full due of what it has done, and no injustice will be done unto them'
+            end
+        end }
+    }
 })
